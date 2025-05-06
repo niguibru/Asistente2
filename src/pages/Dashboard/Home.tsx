a1ce254007f2af4e8850b0b3287e736916286fc3
wochipin <wochipin@gmail.com>
2025-05-06 13:14:44 +0200

    Initial commit

---
+++ src/pages/Dashboard/Home.tsx
@@ -0,0 +1,41 @@
+import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
+//import RecentOrders from "../../components/ecommerce/RecentOrders";
+import PageMeta from "../../components/common/PageMeta";
+import ChatAgent from "../../components/chatAgent/ChatAgent";
+
+export default function Home() {
+  return (
+    <>
+      <PageMeta
+        title="Portal Zinco "
+        description=""
+      />
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
+        <div className="col-span-12 space-y-6 xl:col-span-7">
+          <ChatAgent />
+
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          <EcommerceMetrics />
+        </div>
+
+        {/* <div className="col-span-12 xl:col-span-5">
+          <MonthlyTarget />
+        </div>
+
+        <div className="col-span-12">
+          <StatisticsChart />
+        </div> */}
+
+        {/* <div className="col-span-12 xl:col-span-5">
+          <DemographicCard />
+        </div>
+
+        <div className="col-span-12 xl:col-span-7">
+          <RecentOrders />
+        </div> */}
+      </div>
+    </>
+  );
+}
